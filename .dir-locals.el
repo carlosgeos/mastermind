ce4661d8849c661a5633b3753998852506b9dbbf
Carlos Requena López <crequena@ulb.ac.be>
2018-12-10 19:23:23 +0100

    OpenMPI hello world to test on hydra
    Makefile contains directives to build, run etc

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-disabled-checkers c/c++-cppcheck)
+  (flycheck-c/c++-gcc-executable . "/usr/bin/mpic++")))
