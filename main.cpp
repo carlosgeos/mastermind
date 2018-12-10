942370a57071702669c9879d7c918262eb3ec1e1
Théo Verhelst <tverhels@ulb.ac.be>
2018-12-10 22:39:03 +0100

    Properly format C++

--- main.cpp
+++ main.cpp
@@ -4,80 +4,63 @@
 # include <iostream>
 # include <mpi.h>
 
-using namespace std;
+void timestamp();
 
-int main ( int argc, char *argv[] );
-void timestamp ( );
+int main(int argc, char *argv[]) {
+    int ierr{MPI_Init(&argc, &argv)};
 
-int main ( int argc, char *argv[] )
+    if (ierr != 0) {
+        std::cout << "\n";
+        std::cout << "HELLO_MPI - Fatal error!\n";
+        std::cout << "MPI_Init returned nonzero ierr.\n";
+        return 1;
+    }
 
-{
-  int id;
-  int ierr;
-  int p;
-  double wtime;
+    int p;
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-  ierr = MPI_Init(&argc, &argv);
+    int id;
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-  if ( ierr != 0 ) {
-    cout << "\n";
-    cout << "HELLO_MPI - Fatal error!\n";
-    cout << "  MPI_Init returned nonzero ierr.\n";
-    exit ( 1 );
-  }
+    if (id == 0) {
+        timestamp();
+        std::cout << "\n";
+        std::cout << "P" << id << ":  HELLO_MPI - Master process:\n";
+        std::cout << "P" << id << ":    C++/MPI version\n";
+        std::cout << "P" << id << ":    An MPI example program.\n";
+        std::cout << "\n";
+        std::cout << "P" << id << ":    The number of processes is " << p << "\n";
+        std::cout << "\n";
+    }
 
-  ierr = MPI_Comm_size ( MPI_COMM_WORLD, &p );
+    double wtime;
+    if (id == 0) {
+        wtime = MPI_Wtime();
+    }
+    std::cout << "P" << id << ":    'Hello, world!'\n";
 
-  ierr = MPI_Comm_rank ( MPI_COMM_WORLD, &id );
+    if (id == 0) {
+        wtime = MPI_Wtime() - wtime;
+        std::cout << "P" << id << ":    Elapsed wall clock time = " << wtime << " seconds.\n";
+    }
 
-  if ( id == 0 ) {
-    timestamp ( );
-    cout << "\n";
-    cout << "P" << id << ":  HELLO_MPI - Master process:\n";
-    cout << "P" << id << ":    C++/MPI version\n";
-    cout << "P" << id << ":    An MPI example program.\n";
-    cout << "\n";
-    cout << "P" << id << ":    The number of processes is " << p << "\n";
-    cout << "\n";
-  }
+    MPI_Finalize();
 
-  if ( id == 0 ) {
-    wtime = MPI_Wtime ( );
-  }
-  cout << "P" << id << ":    'Hello, world!'\n";
-
-  if ( id == 0 ) {
-    wtime = MPI_Wtime ( ) - wtime;
-    cout << "P" << id << ":    Elapsed wall clock time = " << wtime << " seconds.\n";
-  }
-
-  MPI_Finalize ( );
-
-  if ( id == 0 ) {
-    cout << "\n";
-    cout << "P" << id << ":  HELLO_MPI:\n";
-    cout << "P" << id << ":    Normal end of execution.\n";
-    cout << "\n";
-    timestamp ( );
-  }
-  return 0;
+    if (id == 0) {
+        std::cout << "\n";
+        std::cout << "P" << id << ":  HELLO_MPI:\n";
+        std::cout << "P" << id << ":    Normal end of execution.\n";
+        std::cout << "\n";
+        timestamp();
+    }
+    return 0;
 }
-void timestamp ( )
 
-{
-# define TIME_SIZE 40
-
-  static char time_buffer[TIME_SIZE];
-  const struct std::tm *tm_ptr;
-  std::time_t now;
-
-  now = std::time ( NULL );
-  tm_ptr = std::localtime ( &now );
-
-  std::strftime ( time_buffer, TIME_SIZE, "%d %B %Y %I:%M:%S %p", tm_ptr );
-
-  std::cout << time_buffer << "\n";
-
-  return;
-# undef TIME_SIZE
+void timestamp() {
+    constexpr std::size_t buffer_size{64};
+    static char time_buffer[buffer_size];
+    const struct std::tm *tm_ptr;
+    std::time_t now{std::time(nullptr)};
+    std::strftime(time_buffer, buffer_size, "%d %B %Y %I:%M:%S %p",  std::localtime(&now));
+    std::cout << time_buffer << "\n";
 }
